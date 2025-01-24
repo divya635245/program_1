5bd7e62b4547f5f509d3a5e4dd3f64142abe4290
divya635245 <bhadanedivya249@gmail.com>
2025-01-24 13:02:05 +0530

    Create program_1.py

---
+++ program_1.py
@@ -0,0 +1,86 @@
+#1 arithmethic opration
+
+a = 10
+b = 5
+
+print(a + b)
+
+a = 10
+b = 5
+
+print(a - b)
+
+a = 10
+b = 5
+
+print(a * b)
+
+a = 10
+b = 5
+
+print(a % b)
+
+a = 10
+b = 5
+
+print(a / b)
+
+#2 comprison operetors
+
+x = 15
+y = 20
+
+print(x > y)
+
+x = 15
+y = 20
+
+print(x < y)
+
+x = 15
+y = 20
+
+print(x == y)
+
+X = 15
+Y = 20
+
+print(x != y)
+
+x = 15
+y = 20
+
+print(x >= y and y <= x)
+
+# area of circal
+
+r=9
+a= 3.14*r
+print(a)
+
+
+# swape program
+
+a=9
+b=8
+b=a
+
+print(b)
+a=b
+print(a)
+
+# simpal intrest calculations
+
+principal = 5000
+rate = 5
+time = 2
+
+simple_interest = (principal * rate * time ) / 100
+
+print("The Simple Interest is:", simple_interest)
+
+
+
+
+
+
